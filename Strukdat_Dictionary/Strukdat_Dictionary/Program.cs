80cc929157e11fd6a301cc451adcb3dfecae24a2
syahrizalfauzi <44159978+syahrizalfauzi@users.noreply.github.com>
2019-05-23 21:02:05 +0700

    Initial commit
    commit sudoku

---
+++ Strukdat_Dictionary/Strukdat_Dictionary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strukdat_Dictionary
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Dictionary<string, int> d = new Dictionary<string, int>();
+
+			Console.WriteLine("Masukkan kalimat : ");
+			string input = Console.ReadLine();
+			string[] inputArray = input.Split(' ');
+
+			foreach (string s in inputArray)
+			{
+				int jumlah;
+				d.TryGetValue(s, out jumlah);
+				
+				if(!d.ContainsKey(s))
+					d.Add(s, jumlah+1);
+				else{
+					d[s] = jumlah + 1;
+				}
+			}
+
+			d.ToList().ForEach(x => Console.WriteLine(x.Key + " : " + x.Value));
+
+			Console.ReadKey();
+		}
+	}
+}
